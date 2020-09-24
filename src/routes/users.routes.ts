41c6863186752fb97c41fef45a1c13110298db53
Tarciano-dias <tarciano.dias13@gmail.com>
2020-09-24 00:26:27 -0300

    :construction: Configurando lib multer para realizar upload de arquivos

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,6 +1,11 @@
 import { Router } from 'express';
-import CreateUserService from '../services/CreateUserService'
+import CreateUserService from '../services/CreateUserService';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
+
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
 
@@ -20,5 +25,12 @@ usersRouter.post('/', async (request, response) => {
         return response.status(400).json({ message: error.message });
     }
 })
+
+
+usersRouter.patch('/avatar', ensureAuthenticated, upload.single('avatar'), async (request, response) => {
+
+    
+    return response.json({ ok: true });
+});
 
 export default usersRouter;
